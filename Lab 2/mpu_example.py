43841c3678fa4ca38507289a6aa1e30921c415bb
Hortense Gimonet <hg332@cornell.edu>
2021-03-05 16:33:34 -0500

    modified mpu_example.py to get velocity

--- Lab 2/mpu_example.py
+++ Lab 2/mpu_example.py
@@ -2,13 +2,24 @@ import time
 import board
 import busio
 import adafruit_mpu6050
+import numpy as np
 
 i2c = busio.I2C(board.SCL, board.SDA)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
+
+v = np.zeros(3)
+dt = 1
+
 while True:
+    # calculate velocity
+    v += np.array(mpu.acceleration)*dt
+    speed = np.linalg.norm(v)
+
     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(mpu.acceleration))
+    print("Speed: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (v))
     print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s"%(mpu.gyro))
     print("Temperature: %.2f C"%mpu.temperature)
     print("")
-    time.sleep(1)
+
+    time.sleep(dt)
