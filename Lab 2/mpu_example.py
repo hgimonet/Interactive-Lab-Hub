085758ef266cd09b8e95d9fe7653db52015abd14
Hortense Gimonet <hg332@cornell.edu>
2021-03-08 09:48:55 -0500

    added calibration offsets

--- Lab 2/mpu_example.py
+++ Lab 2/mpu_example.py
@@ -7,19 +7,24 @@ import numpy as np
 i2c = busio.I2C(board.SCL, board.SDA)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
-mpu.reset()
+accel_offsets = [ 0.78548489, -0.04208049,  7.99242454]
+gyro_offsets = [-2.38952672, -0.4019084,  -0.25210687]
 
 v = np.zeros(3)
 dt = 0.010
 
+
+
 while True:
+    accel = np.array(mpu.acceleration) - accel_offsets
+    gyro = np.array(mpu.gyro) - gyro_offsets
     # calculate velocity
-    v += np.array(mpu.acceleration)*dt
+    v += accel*dt
     speed = np.linalg.norm(v)
 
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(mpu.acceleration))
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(tuple(accel)))
     print("Speed: X:%.2f, Y: %.2f, Z: %.2f m/s^2" %(tuple(v)))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s"%(mpu.gyro))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s"%(tuple(gyro)))
     print("Temperature: %.2f C"%mpu.temperature)
     print("")
 
