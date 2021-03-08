7ac8f9e0aa181ffe65e519858af123cf116e1cc6
Hortense Gimonet <hg332@cornell.edu>
2021-03-08 09:56:17 -0500

    longer calibration time

--- Lab 2/mpu_calibrate.py
+++ Lab 2/mpu_calibrate.py
@@ -9,8 +9,8 @@ mpu = adafruit_mpu6050.MPU6050(i2c)
 
 # mpu.reset()
 
-dt=0.001
-secs = 10.0
+dt=0.01
+secs = 5.0
 num_measurements = int(secs/dt)
 count = 0
 
@@ -19,21 +19,11 @@ gyro_recordings = []
 
 print('Will measure for %.1f seconds, do not move sensor.'%(secs))
 while count < num_measurements:
-    # print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(mpu.acceleration))
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(mpu.acceleration))
     accel_recordings.append(np.array(mpu.acceleration))
     gyro_recordings.append(np.array(mpu.gyro))
     time.sleep(dt)
     count +=1
 
-# accel_recordings = np.array(accel_recordings)
-# accel_recordings = np.array(accel_recordings)
 print(np.array(accel_recordings).mean(axis=0))
 print(np.array(gyro_recordings).mean(axis=0))
-
-# while True:
-#     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(mpu.acceleration))
-#     print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s"%(mpu.gyro))
-#     # print("Temperature: %.2f C"%mpu.temperature)
-#     print("")
-#
-#     time.sleep(1)
