dc7eda9da42209aab63d34dcd7e3d2d64871b326
Hortense Gimonet <hg332@cornell.edu>
2021-03-22 01:19:53 -0400

    debug

--- Lab 3/wizard/app.py
+++ Lab 3/wizard/app.py
@@ -84,12 +84,41 @@ draw = ImageDraw.Draw(image)
 draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
 disp.image(image)
 
+
+def diplay_img(image):
+    backlight = digitalio.DigitalInOut(board.D22)
+    backlight.switch_to_output()
+    backlight.value = True
+
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image)
+
+
 def play_sequence(action):
     frames = pup.FRAMES[action]
     for frame in frames:
         print(frame.width, frame.height)
-        disp.image(frame.rotate(90))
+        diplay_img(frame)
+        # disp.image(frame.rotate(90))
         time(0.1)
+
+
 
 play_sequence('walk_front')
 
