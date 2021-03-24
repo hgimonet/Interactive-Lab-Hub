9fd65121a89b137028021a1cb1de8065406e8cbe
Hortense Gimonet <hg332@cornell.edu>
2021-03-23 20:15:46 -0400

    added dependencies

--- Lab 4/pi_shutdown.py
+++ Lab 4/pi_shutdown.py
@@ -4,11 +4,16 @@ import board
 import time  # import time functions
 import os  # imports OS library for Shutdown control
 
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
 stopButton = digitalio.DigitalInOut(board.D23)  # defines the button as an object and chooses GPIO 26
 stopButton.switch_to_input()
 
 while True:
-    time.sleep(1)  # wait to loop again so we don’t use the processor too much.
+    time.sleep(1)  # wait to loop again so we dont use the processor too much.
     if stopButton:  # Check to see if button is pressed
         time.sleep(1)  # wait for the hold time we want.
         if stopButton:
