1d6eb4ad1b135ab37ef042ee89c178bf648c9c11
Hortense Gimonet <hg332@cornell.edu>
2021-03-24 17:43:56 -0400

    lick count try 1

--- Lab 4/lick_count.py
+++ Lab 4/lick_count.py
@@ -11,5 +11,14 @@ lcd = LDC.lcd()
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
+BOWL_ID = 5
+BUTTON_ID = 6
+
 lick_count = 0
 while True:
+    if mpr121[BUTTON_ID].value:
+        lick_count = 0
+    if mpr121[BOWL_ID].value:
+        lick_count +=1
+    lcd.lcd_write('Lick Count: %4d'%(lick_count))
+    time.sleep(1)
