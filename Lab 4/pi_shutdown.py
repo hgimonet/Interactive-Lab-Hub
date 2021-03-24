850ba1f46a633b35c5080bf8bd2d1f945a604548
Hortense Gimonet <hg332@cornell.edu>
2021-03-23 20:11:01 -0400

    fixed indentations

--- Lab 4/pi_shutdown.py
+++ Lab 4/pi_shutdown.py
@@ -1,14 +1,15 @@
 # import what is needed for the shutdown button
 import digitalio
 import board
-import time # import time functions
-import os #imports OS library for Shutdown control
+import time  # import time functions
+import os  # imports OS library for Shutdown control
 
 stopButton = digitalio.DigitalInOut(board.D23) # defines the button as an object and chooses GPIO 26
+stopButton.switch_to_input()
 
 while True: #infinite loop
-     if stopButton.is_pressed: #Check to see if button is pressed
+     if stopButton: #Check to see if button is pressed
         time.sleep(1) # wait for the hold time we want.
-        if stopButton.is_pressed: #check if the user let go of the button
+        if stopButton:
             os.system("shutdown now -h") #shut down the Pi -h is or -r will reset
     time.sleep(1) # wait to loop again so we don’t use the processor too much.
